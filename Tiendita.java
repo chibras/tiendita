a782e710735bd6bd7db38d94badc01868f258e92
gabyhg <gabyhernandezgarcia3@gmail.com>
2016-04-12 22:46:02 -0500

    Added files via upload

---
+++ Tiendita.java
@@ -0,0 +1,17 @@
+public class Tiendita{
+
+	public static void main(String args[]){
+		println("Bienvenido a tu Tiendita");
+		
+
+
+
+	}
+}
+
+public class Producto{
+	protected String prod;
+	protected int precio;
+	protected int cantidad;
+
+}
